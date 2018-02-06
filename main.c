ccec42ca851747825a6feb89ab7d34307c87667f
Brian Wong <brwwong@cs.stonybrook.edu>
2018-02-05 22:17:43 -0800

    wrote helper string compare function

--- main.c
+++ main.c
@@ -1,14 +1,29 @@
 #include <stdio.h>
 
+//function to check if 2 strings are equal
+//returns 0 if strings are not equal, 1 if they are
+int checkStrings(char *str1.char *str2){
+  while(*str1!='\0'){
+  	if(*str1!=*str2)
+		return 0;
+	str1++;
+	str2++;
+  }
+  if (str2!='\0')
+  	return 0;
+  return 1;
+}
 int main(int argc, char** argv) {
   /*
   * Dummy values
   */
-  int id = 1;
-  char* first_name = "Sergey";
-  char* last_name = "Madaminov";
-  float gpa = 3.41;
-  char* major = "CSE";
+  int id;
+  char* first_name;
+  char* last_name;
+  float gpa;
+  char* major;
+  //start with the first argument 
+  argv++;
   /*
   * This formatting for the string
   * that you are expected to follow
